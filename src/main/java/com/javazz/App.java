c179a5f149690dbf2ab5455f699120827f4041be
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-21 09:39:48 +0530

    initial message

---
+++ src/main/java/com/javazz/App.java
@@ -0,0 +1,16 @@
+package com.javazz;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	LOG.info("Hello, World");
+    }
+}
